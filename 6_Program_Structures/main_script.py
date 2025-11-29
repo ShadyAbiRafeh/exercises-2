b482f408ddb51b35c2e54a1b2cabe6807d96417d
Shady Abi Rafeh <abirafeh.s.06@gmail.com>
2025-11-29 09:47:43 +0100

    code

---
+++ 6_Program_Structures/main_script.py
@@ -0,0 +1,8 @@
+import utils
+
+length = 5
+width = 3
+area = utils.calculate_area(length, width)
+perimeter = utils.calculate_perimeter(length, width)
+print(f"Area: {area}")
+print(f"Perimeter: {perimeter}")
